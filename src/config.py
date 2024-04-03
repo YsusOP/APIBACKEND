aa97c55ed380f025338edcf816de361345fd65fe
YsusOP <117765256+YsusOP@users.noreply.github.com>
2024-04-03 18:35:14 -0500

    z

--- src/config.py
+++ src/config.py
@@ -1,11 +1,18 @@
-class DevelomentConfig():
-    DEBUG=True
-    MYSQL_HOST='basededatos.mysql.database.azure.com'
-    MYSQL_USER='exercise'
-    MYSQL_PASSWORD='57951227Pumba'
-    MYSQL_DB='mydb'
+class DevelopmentConfig:
+    DEBUG = True
+    MYSQL_HOST = 'basededatos.mysql.database.azure.com'
+    MYSQL_USER = 'exercise'
+    MYSQL_PASSWORD = '57951227Pumba'
+    MYSQL_DB = 'mydb'
 
+class ProductionConfig:
+    DEBUG = False
+    MYSQL_HOST = 'basededatos.mysql.database.azure.com'
+    MYSQL_USER = 'exercise'
+    MYSQL_PASSWORD = '57951227Pumba'
+    MYSQL_DB = 'mydb'
 
-config={
+config = {
+    'development': DevelopmentConfig,
     'production': ProductionConfig
 }
